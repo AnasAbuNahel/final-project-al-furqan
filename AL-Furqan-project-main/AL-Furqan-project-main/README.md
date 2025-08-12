5e3c566a56f2467b81da1966987c87a6432b14fa
Anas Abu Nahel <125291685+AnasAbuNahel@users.noreply.github.com>
2025-08-12 22:55:42 +0300

    Delete AL-Furqan-project-main/AL-Furqan-project-main directory

--- AL-Furqan-project-main/AL-Furqan-project-main/README.md
+++
@@ -1,1 +0,0 @@
-# AL-Furqan-project
--- AL-Furqan-project-main/AL-Furqan-project-main/backend/reset_db.py
+++
@@ -1,7 +0,0 @@
-from app import app, db
-from models import Resident, User
-
-with app.app_context():
-    db.drop_all()     # حذف جميع الجداول
-    db.create_all()   # إنشاءها من جديد
-    print("✅ تم تفريغ قاعدة البيانات وإنشاؤها من جديد.")
